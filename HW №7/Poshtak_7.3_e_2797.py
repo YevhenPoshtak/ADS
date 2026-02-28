46ec1b016be164e14793f5f20c8d8731e5063b18
YevhenPoshtak <yevhenposhtak@knu.ua>
2026-02-28 22:16:35 +0200

    Refactor Counter class to use hash table

--- HW №7/Poshtak_7.3_e_2797.py
+++ HW №7/Poshtak_7.3_e_2797.py
@@ -1,20 +1,30 @@
 class Counter:
-    def __init__(self, iterable) -> None:
-        self.table = {}  
+    def __init__(self, iterable: list[str]) -> None:
+        self.capacity = 100003
+        self.table = [[] for _ in range(self.capacity)]
         if iterable:
             for item in iterable:
                 self.add(item)
+
+    def _hash(self, key: str) -> int:
+        h = 0
+        for char in str(key):
+            h = (h * 31 + ord(char)) % self.capacity
+        return h
     
     def add(self, item: str) -> None:
-        if item in self.table:
-            self.table[item] += 1
-        else:
-            self.table[item] = 1
-    
+        index = self._hash(item)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == item:
+                self.table[index][i] = (item, pair[1] + 1)
+                return
+        self.table[index].append((item, 1))
+
     def keys(self) -> list:
         result = []
-        for key in self.table:  
-            result.append(key)
+        for bucket in self.table:
+            for k, v in bucket:
+                result.append(k)
         return result
 
 class Solution:
